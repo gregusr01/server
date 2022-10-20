bf0089ec180e279fea2f04844e765c6c8bdffcf0
RyanGregus <ryan.gregus@target.com>
2022-10-20 15:01:12 -0500

    tokenmanager

---
+++ tokenmanager/tokenmanager.go
@@ -0,0 +1,105 @@
+package tokenmangager
+
+import (
+	"crypto/rsa"
+	"errors"
+	"time"
+  "fmt"
+
+	"github.com/golang-jwt/jwt"
+)
+
+type AuthClaims struct {
+  TokenType   string
+	Iat         time.Time
+  Exp         time.Time
+  Sub         string
+}
+
+func mintToken(tokenType, hostname string, signKey *rsa.PrivateKey) (string, error) {
+
+  //pull priv key from postgres
+
+
+  switch TokenType: {
+  case: "Registration"
+    tk := jwt.NewWithClaims(jwt.SigningMethodRS256, jwt.MapClaims{
+      "tokenType":    tokenType,
+      "iat":          time.Now().Unix(),
+      "exp":          time.Now().Unix(),
+      "sub":          hostname,
+    })
+  case: "Auth"
+    tk := jwt.NewWithClaims(jwt.SigningMethodRS256, jwt.MapClaims{
+      "tokenType":    tokenType,
+      "iat":          time.Now().Unix(),
+      "exp":          time.Now().Unix(),
+      "sub":          hostname,
+    })
+  default:
+    return nil, errors.New("invalid token type")
+  }
+
+  token, err := tk.SignedString(signKey)
+  if err != nil {
+    return nil, err //wrap error
+  }
+
+  return token, nil
+}
+
+// validateToken validates a token using the public key
+func validateToken(token string, key *rsa.PublicKey) (*AuthClaims, error) {
+
+  //pull pub key from postgres (or use priv key and derive pub)
+
+	tkn, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		return key, nil
+	})
+	if err != nil {
+		return nil, errors.New("failed parsing: " + err.Error())
+	}
+	if !tkn.Valid {
+		return nil, errors.New("invalid token")
+	}
+
+  //validate exp time
+
+
+  //validate not part of invalidationDB
+
+
+	return parseAuthClaims(tkn)
+}
+
+// parseAuthClaims parses jwtAuthN claims post signature validation.
+func parseAuthClaims(token *jwt.Token) (*AuthClaims, error) {
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return nil, errors.New("could not create token claims")
+	}
+
+	iatTime, ok := claims["iat"].(float64)
+	if !ok {
+		return nil, errors.New("iat claim is of invalid type")
+	}
+
+  expTime, ok := claims["exp"].(float64)
+  if !ok {
+    return nil, errors.New("exp claim is of invalid type")
+  }
+
+	return &AuthClaims{
+    TokenType:   claims["tokenType"].(string),
+		Iat:         time.Unix(int64(iatTime), 0),
+    Exp:         time.Unix(int64(expTime), 0),
+    Sub:         claims["sub"].(string),
+	}, nil
+}
+
+
+// func invalidateToken(){
+//
+//   //drop in invalidation db
+//
+// }
