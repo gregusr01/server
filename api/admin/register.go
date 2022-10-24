25d03001385154a5ce65f75c828949a1a732e86b
adam <adam.kiheri@target.com>
2022-10-24 10:00:18 -0500

    updated registration

--- api/admin/register.go
+++ api/admin/register.go
@@ -5,32 +5,49 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-vela/server/router/middleware/org"
+	"github.com/go-vela/server/router/middleware/user"
+	"github.com/go-vela/server/scm"
 	"github.com/go-vela/server/tokenmanager"
 	"github.com/go-vela/server/util"
+	"github.com/sirupsen/logrus"
 )
 
-type Message struct {
-	message string
+type AuthToken struct {
+	JWTToken string
 }
 
 func Register(c *gin.Context) {
 
-	tk := c.Request.Header.Get("Authorization")
-	claims, err := tokenmanager.ValidateToken(tk)
+	// validate User via git token
+	// return registration token
+
+	o := org.Retrieve(c)
+	u := user.Retrieve(c)
+
+	c.Request.ParseForm()
+
+	hn := c.Request.Form.Get("hostname")
+
+	perm, err := scm.FromContext(c).OrgAccess(u, o)
+	if err != nil {
+		logrus.Errorf("unable to get user %s access level for org %s", u.GetName(), o)
+	}
+
+	if perm != "admin" {
+		//reject
+	}
+
+	ntk, err := tokenmanager.MintToken("Registration", hn)
 	if err != nil {
 		retErr := fmt.Errorf("unable to validate token for registration: %s", err)
 		util.HandleError(c, http.StatusUnauthorized, retErr)
 		return
 	}
 
-	//Db call to update registration token
-
-	ntk, err := tokenmanager.MintToken("Auth", claims.Sub)
-	if err != nil {
-		retErr := fmt.Errorf("unable to validate token for registration: %s", err)
-		util.HandleError(c, http.StatusUnauthorized, retErr)
-		return
+	at := AuthToken{
+		JWTToken: ntk,
 	}
 
-	c.JSON(http.StatusOK, Message{message: "worker successfully registered"})
+	c.JSON(http.StatusOK, at)
 }
