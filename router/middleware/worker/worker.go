aec2906ddcd3884d7a146748655575b1d7201884
RyanGregus <ryan.gregus@target.com>
2022-11-28 13:42:41 -0600

    establish worker

--- router/middleware/worker/worker.go
+++ router/middleware/worker/worker.go
@@ -45,3 +45,40 @@ func Establish() gin.HandlerFunc {
 		c.Next()
 	}
 }
+
+// Establish sets the worker in the given context using auth token.
+func EstablishWithAuth() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		wParam := util.PathParameter(c, "worker")
+		if len(wParam) == 0 {
+			retErr := fmt.Errorf("no worker parameter provided")
+			util.HandleError(c, http.StatusBadRequest, retErr)
+
+			return
+		}
+
+		logrus.Debug("Comparing provided worker from token", wt)
+
+		wt := FromContext(c)
+		hn := wt.Hostname
+		if hn != wParam {
+			retErr := fmt.Errorf("Worker %s is not authorized to perform this request for %s", hn, wParam)
+			util.HandleError(c, http.StatusUnauthorized, retErr)
+
+			return
+		}
+
+		logrus.Debugf("Reading worker %s", wParam)
+
+		w, err := database.FromContext(c).GetWorker(wParam)
+		if err != nil {
+			retErr := fmt.Errorf("unable to read worker %s: %w", wParam, err)
+			util.HandleError(c, http.StatusNotFound, retErr)
+
+			return
+		}
+
+		ToContext(c, w)
+		c.Next()
+	}
+}
